711ee65f1f05436ce123daed550acbe843a951a1
lachake <lachake@vbox.myguest.virtualbox.org>
2026-01-13 01:44:54 +0000

    Basic Scripts

---
+++ aliases.sh
@@ -0,0 +1,27 @@
+# File for all the Aliases that I use on 
+# the daily.
+
+# Linux
+alias ll="ls -alF"
+alias c="clear"
+
+# Package Management
+alias update="sudo apt update && sudo apt upgrade"
+
+# Git 
+
+alias gi="git init"
+alias ga="git add"
+
+alias gs="git status"
+alias gc="git commit -m "
+
+alias gps="git push"
+alias gpl="git pull"
+
+alias gb="git branch"
+alias gbc="git checkout -b"
+alias gbd="git branch -D"
+
+alias glo="git log --oneline --decorate"
+alias gl="git log"
