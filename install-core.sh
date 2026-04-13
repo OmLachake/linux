c1062e8600f6fef1a9d74a8a246e1c0ce8f88b98
Om Lachake <omlachake@pm.me>
2026-04-13 18:58:56 +0100

    Updated the core tools

--- install-core.sh
+++ install-core.sh
@@ -3,9 +3,9 @@
 # Install Ghostty Terminal
 # https://copr.fedorainfracloud.org/coprs/scottames/ghostty/
 
-sudo dnf copr enable scottames/ghostty
+sudo dnf copr enable scottames/ghostty 
 sudo dnf check-updates
-sudo dnf install ghostty
+sudo dnf install ghostty -y
 
 
 # Install VSCode
@@ -13,16 +13,19 @@ sudo dnf install ghostty
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 sudo dnf check-update 
-sudo dnf install code # or code-insiders
-
+sudo dnf install code git curl -y 
+sudo dnf install neovim
 
 # Containerization Tools
 
-sudo dnf install docker docker-compose kubernetes
-
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin kubernetes -y
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
 
 # AI Tools
 
 curl -fsSL https://claude.ai/install.sh | bash
 
-
+# Other tools
+flatpak install flathub md.obsidian.Obsidian -y
