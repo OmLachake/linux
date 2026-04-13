46a11faa1248451537b548c49229bc41b2014ff9
Om Lachake <omlachake@pm.me>
2026-04-13 19:30:08 +0100

    Fixed missing C/C++ libs

--- install-c.sh
+++ install-c.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
-sudo dnf group install "Development Tools" "C Development Tools and Libraries"
-sudo dnf install gdb valgrind perf strace ltrace cmake ninja-build llvm clang lldb clang-tools-extra  
+sudo dnf install -y \
+  gcc \
+  gcc-c++ \
+  gcc-gdb-plugin \
+  libgcc \
+  libgcc.i686 \
+  libasan \
+  libasan.i686 \
+  libtsan \
+  libubsan \
+  libubsan.i686 \
+  liblsan \
+  libatomic \
+  libatomic.i686 \
+  libgomp \
+  libgomp.i686 \
+  libstdc++ \
+  libstdc++-devel \
+  libstdc++-static \
+  gcc-plugin-devel \
+  cpp
 
+sudo dnf group install "Development Tools" "C Development Tools and Libraries" -y
+sudo dnf install gdb valgrind perf strace ltrace cmake ninja-build llvm clang lldb clang-tools-extra  -y
+
+sudo dnf install -y \
+  libpcap-devel \
+  openssl-devel \
+  libuv-devel \
+  libcurl-devel \
+  c-ares-devel \
+  protobuf-devel \
+  protobuf-compiler \
+  meson \
+  ccache \
+  cppcheck \
+  iwyu \
+  systemtap \
+  hyperfine \
+  doxygen
